b7fdd101070c76057263f4fee3d6fad48b753c5c
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2023-12-21 23:40:20 +0100

    maj_2023_zadanie5

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Integer.parseInt("2011", 3) == Integer.parseInt("134", 6));
+        System.out.println(Integer.parseInt("134", 5) < Integer.parseInt("134", 6));
+        System.out.println(Integer.parseInt("2222", 3) < Integer.parseInt("1111", 6));
     }
 }
