49b03958c002ef82852386aec8413d9136f00feb
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2024-01-22 17:14:23 +0100

    zadania

--- czerwiec_2023/zadanie3_1/src/FileReader.java
+++ czerwiec_2023/zadanie3_1/src/FileReader.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class FileReader {
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileReader {
+    public List<String> ReadFile() throws URISyntaxException, IOException {
+        return Files.readAllLines(Paths.get(this.getClass().getResource("anagram.txt").toURI()));
+    }
 }
--- czerwiec_2023/zadanie3_3/src/Main.java
+++ czerwiec_2023/zadanie3_3/src/Main.java
@@ -1,21 +1,32 @@
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         FileReader fR = new FileReader();
         List<String> lines = fR.readLines();
 
-        for (int i = 0; i < lines.size(); i++) {
-
+        int biggestAbsoluteValue = 0;
+        for (int i = 0; i < lines.size() - 1; i++) {
+            int firstVal = convertToDec(lines.get(i));
+            int secondVal = convertToDec(lines.get(i + 1));
+            int r = Math.abs(firstVal - secondVal);
+            if (r > biggestAbsoluteValue) biggestAbsoluteValue = r;
         }
+
+        System.out.println(Integer.toBinaryString(biggestAbsoluteValue));
+    }
+
+    public static int convertToDec(String s) {
+        return Integer.parseInt(s, 2);
     }
 }
 
 class FileReader {
-    public List<String> readLines() throws IOException {
-        return Files.readAllLines(Paths.get(String.valueOf(getClass().getResource("przyklad.txt"))));
+    public List<String> readLines() throws IOException, URISyntaxException {
+        return Files.readAllLines(Paths.get(this.getClass().getResource("przyklad.txt").toURI()));
     }
 }
