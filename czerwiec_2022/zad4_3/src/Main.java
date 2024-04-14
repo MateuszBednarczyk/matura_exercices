e276f1a607ede886fb1895f4026a013333d91cc4
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2024-04-14 22:33:14 +0200

    zadania_czerwiec22

--- czerwiec_2022/zad4_3/src/Main.java
+++ czerwiec_2022/zad4_3/src/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws URISyntaxException, IOException {
         FileReader fileReader = new FileReader();
-        List<Integer> data =fileReader.readLines().stream().map(Integer::parseInt).toList();
+        List<Integer> data = fileReader.readLines().stream().map(Integer::parseInt).toList();
 
         for (Integer i : data) {
             StringBuilder convertedInt = new StringBuilder(String.valueOf(i));
