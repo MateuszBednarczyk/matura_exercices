1f538feb939ed39bd4da595333911dc1c1c6fad6
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2024-04-14 22:25:37 +0200

    zadania_czerwiec22

---
+++ czerwiec_2022/zad4_3/src/Main.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        FileReader fileReader = new FileReader();
+        List<Integer> data =fileReader.readLines().stream().map(Integer::parseInt).toList();
+
+        for (Integer i : data) {
+            StringBuilder convertedInt = new StringBuilder(String.valueOf(i));
+
+            if (isFirst(i) && isFirst(Integer.parseInt(convertedInt.reverse().toString()))) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isFirst(int n) {
+        int dividers = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                dividers++;
+            }
+        }
+
+        return dividers == 2;
+    }
+}
+
+class FileReader {
+    public List<String> readLines() throws URISyntaxException, IOException {
+        return Files.readAllLines(Paths.get(this.getClass().getResource("liczby.txt").toURI()));
+    }
+}
