c2d14b20d9d9d97fa33219a21fd00386e36a6ac1
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2024-04-21 14:03:53 +0200

    zadania_maj_21

---
+++ maj_2021/zad1_2/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 4999;
+        int d = 0;
+        int mnoznik = 1;
+
+        while (n != 0) {
+            int r = n % 10;
+            d = d + (9 - r) * mnoznik;
+            mnoznik = mnoznik * 10;
+            n /= 10;
+        }
+
+        System.out.println(d);
+    }
+}
--- sandbox/sandbox/src/Main.java
+++ sandbox/sandbox/src/Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String s1 = "132";
-        String s2 = "3111";
+        int n = Integer.parseInt("333", 8);
+        int l = Integer.parseInt("10100101", 2);
+        System.out.println(isBigger(n, l));
+    }
 
-        int i = Integer.parseInt(s1, 4) + Integer.parseInt(s2, 4);
-
-        System.out.println(Integer.toBinaryString(i));
-        System.out.println(Integer.toOctalString(i));
-        System.out.println(Integer.toHexString(i));
-        System.out.println(Integer.toUnsignedString(i, 4));
+    public static boolean isBigger(int n, int l) {
+        return n > l;
     }
 }
